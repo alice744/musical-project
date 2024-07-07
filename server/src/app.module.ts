e8ecdf3f9b779ca53f5c16a0215cc2c264bc4a7b
alice <alice.winner74@gmail.com>
2024-07-08 00:48:48 +0700

    add mongodb and create track

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.o7adeep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    ]
 })
 export class AppModule {}
---
+++ server/src/track/track.service.ts
@@ -0,0 +1,24 @@
+import { InjectModel } from "@nestjs/mongoose";
+import { Track, TrackDocument } from "./schemas/track.schema";
+import { Model } from "mongoose";
+import { CreateTrackDto } from "./dto/create-track.dto";
+
+
+export class TrackService {
+    constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>) { }
+
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const track = await this.trackModel.create({ ...dto, listens: 0 })
+        return track;
+    }
+
+    async getAll() {
+    }
+
+    async getOne() {
+    }
+
+    async delete() {
+
+    }
+}
